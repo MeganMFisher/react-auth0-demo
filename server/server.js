8a85e0185db440250633b799ad42f2d333501814
MeganMFisher <meganmichelle318@gmail.com>
2017-07-19 14:17:45 -0600

    everything working

--- server/server.js
+++ server/server.js
@@ -31,77 +31,77 @@ massive(config.database).then ((db) => {
 // DATABASE //
 /////////////
 
-// passport.use(new Auth0Strategy({
-//     domain: config.domain,
-//     clientID: config.clientID,
-//     clientSecret: config.clientSecret,
-//     callbackURL: '/auth/callback'
-//   },
-//   function (accessToken, refreshToken, extraParams, profile, done) {
+passport.use(new Auth0Strategy({
+    domain: config.auth0.domain,
+    clientID: config.auth0.clientID,
+    clientSecret: config.auth0.clientSecret,
+    callbackURL: '/auth/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
 
-//     var db = app.get('db')
-//     //Find user in database
-//     db.getUserByAuthId(profile.id).then(function (user) {
-//       user = user[0];
-//       if (!user) { //if there isn't one, we'll create one!
-//         console.log('CREATING USER');
-//         db.createUserByAuth([profile.displayName, profile.id]).then(function (user) {
-//           console.log('USER CREATED', user);
-//           return done(null, user[0]); // GOES TO SERIALIZE USER
-//         })
-//       } else { //when we find the user, return it
-//         console.log('FOUND USER', user);
-//         return done(null, user);
-//       }
-//     })
-//   }
-// ));
+    var db = app.get('db')
+    //Find user in database
+    db.getUserByAuthId(profile.id).then(function (user) {
+      user = user[0];
+      if (!user) { //if there isn't one, we'll create one!
+        console.log('CREATING USER');
+        db.createUserByAuth([profile.displayName, profile.id]).then(function (user) {
+          console.log('USER CREATED', user);
+          return done(null, user[0]); // GOES TO SERIALIZE USER
+        })
+      } else { //when we find the user, return it
+        console.log('FOUND USER', user);
+        return done(null, user);
+      }
+    })
+  }
+));
 
-// //THIS IS INVOKED ONE TIME TO SET THINGS UP
-// passport.serializeUser(function (userA, done) {
-//   console.log('serializing', userA);
-//   var userB = userA;
-//   //Things you might do here :
-//   //Serialize just the id, get other information to add to session, 
-//   done(null, userB); //PUTS 'USER' ON THE SESSION
-// });
+//THIS IS INVOKED ONE TIME TO SET THINGS UP
+passport.serializeUser(function (userA, done) {
+  console.log('serializing', userA);
+  var userB = userA;
+  //Things you might do here :
+  //Serialize just the id, get other information to add to session, 
+  done(null, userB); //PUTS 'USER' ON THE SESSION
+});
 
-// // USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
+// USER COMES FROM SESSION - THIS IS INVOKED FOR EVERY ENDPOINT
 
-// passport.deserializeUser(function (userB, done) {
-//   var userC = userB;
-//   app.get('db').getUserAndFavs(userC.authid).then(function (favorites){
-//     userC.favorites = favorites;
-//   done(null, userC);
-// //move done() inside of promise .then or else it will fire before it gets a response
-//   })
-// });
-
-
+passport.deserializeUser(function (userB, done) {
+  var userC = userB;
+  app.get('db').getUserAndFavs(userC.authid).then(function (favorites){
+    userC.favorites = favorites;
+  done(null, userC);
+//move done() inside of promise .then or else it will fire before it gets a response
+  })
+});
 
 
-// app.get('/auth', passport.authenticate('auth0'));
 
 
-// app.get('/auth/callback',
-//   passport.authenticate('auth0', {
-//     successRedirect: 'http://localhost:4000' //frontend port
-//   }),
-//   function (req, res) {
+app.get('/auth', passport.authenticate('auth0'));
+
+
+app.get('/auth/callback',
+  passport.authenticate('auth0', {
+    successRedirect: 'http://localhost:4000' //frontend port
+  }),
+  function (req, res) {
     
-//     res.status(200).send(req.user);
-//   })
+    res.status(200).send(req.user);
+  })
 
-// app.get('/auth/me', function (req, res) {
-//   if (!req.user) return res.sendStatus(404);
-//   //THIS IS WHATEVER VALUE WE GOT FROM userC variable above.
-//   res.status(200).send(req.user);
-// })
+app.get('/auth/me', function (req, res) {
+  if (!req.user) return res.sendStatus(404);
+  //THIS IS WHATEVER VALUE WE GOT FROM userC variable above.
+  res.status(200).send(req.user);
+})
 
-// app.get('/auth/logout', function (req, res) {
-//   req.logout();
-//   res.redirect('/');
-// })
+app.get('/auth/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+})
 
 
 app.post('/postFav', controller.postFav)
