23e6ab2a9266e9a2e78fa345ded5aa2878410d77
MeganMFisher <meganmichelle318@gmail.com>
2017-07-19 20:04:11 -0600

    get options working!

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getUser } from './services/user';
+import Option from './components/options';
 import './App.css';
 
 class App extends Component {
@@ -11,12 +12,27 @@ class App extends Component {
         }
     }
 
+    componentWillMount() {
+    getUser().then(user => {
+      this.setState({
+        user: user
+      })
+    })
+    console.log(this.state.user)
+    }
+
   render() {
     return (
       <div>
         <button className="loginButton">
             <a href="http://localhost:4005/auth">Login with Auth0</a>
         </button>
+
+        <h2>Welcome</h2>
+
+        <div>
+          <Option />
+        </div>
       </div>
     );
   }
---
+++ app/src/components/options.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { getOptions } from '../services/user';
+
+export default class Store extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            options: [],
+        }
+    }
+
+    componentDidMount() {
+    getOptions().then(options => {
+      this.setState({
+        options: options
+      })
+      console.log(this.state.options)
+    })
+}
+
+    render() {
+
+        const options = this.state.options.map((option, i) => (
+            <ul key={i}>
+                <h3>{ option.option }</h3>
+            </ul>
+        ))
+
+        return(
+            <div>
+            <div>
+                { options }
+             </div>
+             </div>
+         )
+     }
+
+
+
+
+
+
+}
